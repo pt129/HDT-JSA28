17c612da1880df69c7cd5df3ec905daffc155e74
pt129 <npt120907@gmail.com>
2023-08-05 15:32:14 +0700

    new code ss10

---
+++ duancuoikhoa/js/dangki.js
@@ -0,0 +1,58 @@
+let register = document.getElementById("register-form");
+register.addEventListener("submit", function (event) {
+  event.preventDefault(); //// Ngăn sự kiện reload mặc định của trình duyệt.
+
+  let lowerCaseLetter = /[a-z]/g;
+  let upperCaseLetter = /[A-Z]/g;
+  let numbers = /[0-9]/g;
+
+  // in ra email và pass người dùng nhập được nhé
+  const data = {
+    email: register.email.value.trim(),
+    password: register.password.value.trim(),
+  }
+
+  if (data.email.length == 0 && data.password.length == 0) {
+    alert("Please input your email & password");
+  }
+  else if (data.password.length < 8) {
+    alert("Password must be at least 8 characters");
+  }
+  else if (!data.password.match(lowerCaseLetter)) {
+    alert("Password must be contain a lowercase letter");
+  }
+  else if (!data.password.match(upperCaseLetter)) {
+    alert("Password must be contain a uppercase letter");
+  }
+  else if (!data.password.match(numbers)) {
+    alert("Password must be contain a number or special character");
+  }
+  else {
+    // Xử lý với LocalStorage
+    if (localStorage.users) {
+      // Kiểm tra nếu trên localStorage đã có khóa: users
+      let users = JSON.parse(localStorage.users);
+
+      let dem = 0
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email === data.email) {
+          dem++;
+          alert("Please input another email");
+        }
+      }
+      // Kiểm tra biến dem nếu dem>0 => email này đã bị trùng lăp.
+      if (dem == 0) {
+        users.push(data);
+        localStorage.setItem("users", JSON.stringify(users));
+        location.replace("./dangnhap.html");
+      }
+    }
+    else {
+      localStorage.setItem("users", JSON.stringify(
+        [data]
+      ));
+      location.replace("./dangnhap.html");
+    }
+
+  }
+})
---
+++ duancuoikhoa/js/dangnhap.js
@@ -0,0 +1,25 @@
+let login = document.getElementById("login-form");
+login.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Lấy dữ liệu trên LS về;
+  let users = JSON.parse(localStorage.getItem("users"));
+  console.log("users: ", users);
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === login.email.value.trim()) {
+      if (users[i].password === login.password.value.trim()) {
+        alert("Login Success");
+        window.location.href = "../index.html";
+        break;
+      }
+      else {
+        alert("Wrong password");
+      }
+    }
+  }
+  if (users[users.length - 1].email != login.email.value.trim()) {
+    alert("Wrong email");
+  }
+
+})
